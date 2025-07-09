c7eaaa55aa0ffaf04c6394aa4e3a1c7cf502f5aa
sampandit553 <sameertiwari737697@gmail.com>
2025-07-09 18:04:41 +0530

    b

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,13 +1,6 @@
-'use client'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { ToastContainer } from "react-toastify";
-import Script from "next/script"; // ✅ Import Script
-
-
-
+import Providers from "./provider"; // ✅ Correct import
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,26 +12,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-        
-      >
-        {/* ✅ Razorpay script for client usage */}
-        <Script
-          src="https://checkout.razorpay.com/v1/checkout.js"
-          strategy="afterInteractive"
-        />
-       <Provider store={store}>
-        <ToastContainer position="top-center"/>
-        {children}
-        </Provider>
-     
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
