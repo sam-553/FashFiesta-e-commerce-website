7360d93862103f29a2b5eb53730e3b4031dc2f84
sampandit553 <sameertiwari737697@gmail.com>
2025-07-23 18:43:43 +0530

    n

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
           {children}
-          <AiWrapper />
+          <AiWrapper/>
         </Providers>
       </body>
     </html>
